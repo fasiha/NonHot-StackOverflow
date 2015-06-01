20abb62b672b65d63015e9a05fdb0a064c46ef31
Ahmed Fasih <ahmed.fasih@matrixresearch.org>
2015-06-01 15:52:15 -0400

    Remove Meta distractions too

--- data/client-script.js
+++ data/client-script.js
@@ -1,2 +1,8 @@
-document.getElementById("hot-network-questions").style.visibility="hidden";
+// First, get rid of "Hot Network Questions".
+document.getElementById("hot-network-questions").style.visibility = "hidden";
 
+// Then get rid of "Featured on Meta" and "Hot Meta Posts".
+var classNodes = document.getElementsByClassName("community-bulletin");
+for (k = 0; k < classNodes.length; k++) {
+  classNodes[k].parentNode.removeChild(classNodes[k]);
+}
